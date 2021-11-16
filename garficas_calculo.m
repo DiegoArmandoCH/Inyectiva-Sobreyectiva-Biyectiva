6a5c13326f6205aa85c7c39fb26eda201662499b
DiegoArmandoCH <91149406+DiegoArmandoCH@users.noreply.github.com>
2021-11-16 11:42:14 -0600

    ninguno

---
+++ garficas_calculo.m
@@ -0,0 +1,57 @@
+1
+clear
+
+%Dominio de la función
+x=[-20:1:20];
+
+%Función a plotear
+gx=x.^2
+
+%Plotear funcion
+plot(x, gx)
+____________________________
+2
+clear 
+
+% Dominio de la función
+x=[0:1:40];
+
+% Función a plotear
+hx=x.^2
+
+% Función plotear
+plot (x, hx)
+____________________________
+clear
+
+% Dominio de la función
+x=[-15:1:20];
+
+% Función a plotear
+fx= x.^3
+
+% Función plotear
+graphic (x, fx)
+____________________________
+clear
+
+% Dominio de la función
+x=[-32:1:45];
+
+% Función a plotear
+rx=(x.^3);
+
+% Función plotear
+plot(x, rx)
+____________________________
+clear
+
+% Dominio de la función
+x=[-15:1:20];
+
+% Función a plotear
+fx= x.^3
+
+% Función plotear
+graphic (x, fx)
+____________________________
